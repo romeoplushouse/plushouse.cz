aea6cd79899d44cde8e280956bd068f5d2dad250
romeoplushouse <romeo.b.f.h@gmail.com>
2026-01-06 10:30:48 +0100

    Add files via upload

---
+++ messages.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang = array(
+        // English is the default language, you can change the langugae from the config.php file
+        'EN'=> array (
+            'check_email'=>'Please enter a valid email!',
+            'captcha'=>'Please check the Captcha!',
+            'php_error'=>'Could not send mail! Please check your PHP mail configuration.',
+            'success'=>'Potvrzujeme vzkaz',
+            'subscription'=>'Thank you for your Subscription.',
+            'email_exists'=>'Email Already Exists',
+        )
+    );
+?>
