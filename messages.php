b125e29f326b5d927e7c9d5061dd0506a6ed1273
romeoplushouse <romeo.b.f.h@gmail.com>
2026-01-06 12:06:31 +0100

    Improve accessibility and validation for contact forms

--- messages.php
+++ messages.php
@@ -4,6 +4,12 @@ $lang = array(
         // English is the default language, you can change the langugae from the config.php file
         'EN'=> array (
             'check_email'=>'Please enter a valid email!',
+            'invalid_phone'=>'Zadejte platné telefonní číslo.',
+            'invalid_request'=>'Request must come from Ajax.',
+            'required_field'=>'Vyplňte prosím pole "%s".',
+            'name_length'=>'Jméno musí obsahovat alespoň 2 znaky.',
+            'message_length'=>'Zpráva by měla mít alespoň 10 znaků.',
+            'check_select'=>'Vyberte prosím jednu z možností.',
             'captcha'=>'Please check the Captcha!',
             'php_error'=>'Could not send mail! Please check your PHP mail configuration.',
             'success'=>'Potvrzujeme vzkaz',
